fd4dfb2843211e33b3d6f2a2dadefb9ec934c3b8
Amirhesam1999 <amirhesamhashemi4@gmail.com>
2024-04-10 10:44:45 +0330

    this program for help me write program code and upper speed write code

---
+++ programing/programing.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd /home/hesam/Documents/Hesam/html/forms/input-type/
+echo "Please enter name file : "
+read name
+mkdir $name
+touch $name/index.html
+code $name/index.html
+read stop
+google-chrome $name/index.html
+git add $name/index.html
+echo "Please enter git commit : "
+read com
+git commit -m "$com"
+git push
