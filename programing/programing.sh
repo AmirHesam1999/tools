3ba1c2da4f3769f77e7d751345224e02686a6aa7
Hesam <amirhesamhashemi4@gmail.com>
2024-06-07 12:25:43 -0400

    Updated path

--- programing/programing.sh
+++ programing/programing.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/hesam/Documents/Hesam/Css/tutorial/backgrounds/color
+cd /home/hesam/Documents/Hesam/Css/tutorial/backgrounds/attachment
 echo "Please enter name file : "
 read name
 mkdir $name
