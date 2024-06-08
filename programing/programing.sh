1ed90c08fd2453584512174edce09e9e5afa3494
Hesam <amirhesamhashemi4@gmail.com>
2024-06-08 06:23:30 -0400

    Updated path

--- programing/programing.sh
+++ programing/programing.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/hesam/Documents/Hesam/Css/tutorial/Borders/width
+cd /home/hesam/Documents/Hesam/Css/tutorial/Borders/Color
 echo "Please enter name file : "
 read name
 mkdir $name
