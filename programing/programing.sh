6c8456c2303c36f374093bd144bf2373ecdbac83
Hesam <amirhesamhashemi4@gmail.com>
2024-06-22 07:57:08 -0400

    Update path

--- programing/programing.sh
+++ programing/programing.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/hesam/Documents/Hesam/Css/tutorial/outline
+cd /home/hesam/Documents/Hesam/Css/tutorial/outline/outline
 echo "Please enter name file : "
 read name
 mkdir $name
