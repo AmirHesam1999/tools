23268188dfd92df0facddd034c4a7547cf95bf2f
Hesam <amirhesamhashemi4@gmail.com>
2024-06-22 09:04:41 -0400

    Updated path

--- programing/programing.sh
+++ programing/programing.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/hesam/Documents/Hesam/Css/tutorial/outline/Outline-Width
+cd /home/hesam/Documents/Hesam/Css/tutorial/outline/Outline-Color
 echo "Please enter name file : "
 read name
 mkdir $name
