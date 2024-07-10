9dce452e032f8f53e03a913148cab4f5249f9276
Hesam <amirhesamhashemi4@gmail.com>
2024-07-10 11:01:31 -0400

    Updated path

--- programing/programing.sh
+++ programing/programing.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/hesam/Documents/Hesam/Css/tutorial/text/Decoration
+cd /home/hesam/Documents/Hesam/Css/tutorial/text/Spacing
 echo "Please enter name file : "
 read name
 mkdir $name
