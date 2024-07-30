2d2d090107ab2a1c43e06f28e0f4fca2dfb45013
Hesam <amirhesamhashemi4@gmail.com>
2024-07-30 09:26:25 -0400

    update tools for easy underestand steps script

--- programing/programing.sh
+++ programing/programing.sh
@@ -2,18 +2,21 @@
 echo "Please enter new path : "
 read path
 cd $path
-
+echo "-----------------------------------------------"
 echo "Please enter name file : "
 read name
+echo "-----------------------------------------------"
 mkdir $name
 touch $name/index.html
 code $name/index.html
 read stop
 google-chrome $name/index.html
+echo "-----------------------------------------------"
 git add $name/index.html
 echo "Please enter git commit : "
 read com
 git commit -m "$com"
+echo "-----------------------------------------------"
 git push
  
 
