b01661852883daa521fb14904b5bd478ac59502e
Hesam <amirhesamhashemi4@gmail.com>
2025-01-03 08:24:06 -0500

    Improving the program to reduce processing

--- programing/main.py
+++ programing/main.py
@@ -1,7 +1,14 @@
 #!/bin/python3
 name = input("Enter name: ")
-name = name.replace(" ", "_")
-name = name.replace("/", "_or_")
+i = 0
+i = name .rfind("_")
+if  i != -1:
+    name = name.replace(" ", "_")
+i = 0
+i = name .rfind("/")
+if i != -1:
+    name = name.replace("/", "_or_")
+i = 0
 i = name .rfind("|")
 if i != -1:
     name = name[:i]+ '\\' + name[i:] 
