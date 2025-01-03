68bae5c017c7b6bb7f9181e8516908373a6553ed
Hesam <amirhesamhashemi4@gmail.com>
2025-01-03 09:55:29 -0500

    this program for edit name directory for create directory in terminal linux and change this tools for edit name '()' in value name

--- programing/main.py
+++ programing/main.py
@@ -1,11 +1,25 @@
 #!/bin/python3
+# get value for edit inner text 
 name = input("Enter name: ")
 
+# replace value " " to "_"
 name = name.replace(" ", "_")
 
 name = name.replace("/", "_or_")
+
+# if text has "|" before "|" add "\"
 i = 0
 i = name .rfind("|")
+if i != -1:
+    name = name[:i]+ '\\' + name[i:] 
+
+i = 0
+i = name .rfind("(")
+if i != -1:
+    name = name[:i]+ '\\' + name[i:] 
+
+i = 0
+i = name .rfind(")")
 if i != -1:
     name = name[:i]+ '\\' + name[i:] 
 print(name)
