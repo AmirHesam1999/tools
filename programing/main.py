57b47b1b55cc06222edff9ed7fa52a071e32b2d3
Hesam <amirhesamhashemi4@gmail.com>
2025-01-03 08:26:52 -0500

    restore program

--- programing/main.py
+++ programing/main.py
@@ -1,13 +1,9 @@
 #!/bin/python3
 name = input("Enter name: ")
-i = 0
-i = name .rfind("_")
-if  i != -1:
-    name = name.replace(" ", "_")
-i = 0
-i = name .rfind("/")
-if i != -1:
-    name = name.replace("/", "_or_")
+
+name = name.replace(" ", "_")
+
+name = name.replace("/", "_or_")
 i = 0
 i = name .rfind("|")
 if i != -1:
