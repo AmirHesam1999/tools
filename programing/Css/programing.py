8b228b960511f4fd590f0ea064c4f9561854d56c
Hesam <amirhesamhashemi4@gmail.com>
2025-01-19 06:56:00 -0500

    change command location 'git log' and set better location for this check this command before run program It must be done.

--- programing/Css/programing.py
+++ programing/Css/programing.py
@@ -6,6 +6,7 @@ def clear_commit(name):
 import os 
 path = input("Please enter new path: ")
 os.chdir(path)
+os.system("git log")
 print("----------------------------")
 name = input("Please enter name file : ")
 os.mkdir(name)
@@ -21,6 +22,4 @@ if input("Please enter (y) for run program in terminal : ") == 'y':
     n = clear_commit(n)
     os.system('git commit -m "'+n+'"')
     print("----------------------------")
-    os.system("git log")
-    if input("Are you push git? (y/n): ") == 'y':
-        os.system("git push")
+    os.system("git push")
