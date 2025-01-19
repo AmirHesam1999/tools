8eb675d456c95e7a2ad3666a56706692395314a1
Hesam <amirhesamhashemi4@gmail.com>
2025-01-19 07:35:52 -0500

    I using two function for when test result get error can fix error and run again program

--- programing/Css/programing.py
+++ programing/Css/programing.py
@@ -1,25 +1,39 @@
 #!/bin/python3
+import os 
+def program(b):
+    if b:
+        name = input("Please enter name file : ")
+        os.mkdir(name)
+        os.chdir(name)
+        print("----------------------------")
+        os.system("touch index.html")
+        os.system("code index.html")
+    else:
+        os.system("code index.html")
+        programptwo()
+
+def programptwo():
+    if input("Please enter (y) for run program in terminal : ") == 'y':
+        os.system("google-chrome index.html")
+        n = input("Are you complete program (y/n) : ")
+        if n == 'y':
+            os.system("git add index.html")
+            print("----------------------------")
+            n = input("Please enter git commit : ")
+            n = clear_commit(n)
+            os.system('git commit -m "'+n+'"')
+            print("----------------------------")
+            os.system("git push")
+        elif n == "n":
+            program(False)
 
 def clear_commit(name):
     name = name.replace('"', "'")
     return name
-import os 
+
 path = input("Please enter new path: ")
 os.chdir(path)
 os.system("git log")
 print("----------------------------")
-name = input("Please enter name file : ")
-os.mkdir(name)
-os.chdir(name)
-print("----------------------------")
-os.system("touch index.html")
-os.system("code index.html")
-if input("Please enter (y) for run program in terminal : ") == 'y':
-    os.system("google-chrome index.html")
-    os.system("git add index.html")
-    print("----------------------------")
-    n = input("Please enter git commit : ")
-    n = clear_commit(n)
-    os.system('git commit -m "'+n+'"')
-    print("----------------------------")
-    os.system("git push")
+program(True)
+programptwo()
