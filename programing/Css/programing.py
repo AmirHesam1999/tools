400862eb86202e7cc924a3b2ca097c5f05c22d01
Hesam <amirhesamhashemi4@gmail.com>
2025-06-13 20:10:46 +0330

    I found the bug in this program and made a change so that the program completion does not stop.

--- programing/Css/programing.py
+++ programing/Css/programing.py
@@ -15,18 +15,23 @@ def program(b):
 def programptwo():
     if input("Please enter (y) for run program in terminal : ") == 'y':
         os.system("google-chrome index.html")
-        n = input("Are you complete program (y/n) : ")
-        if n == 'y':
-            os.system("git add index.html")
-            print("----------------------------")
-            n = input("Please enter git commit : ")
-            n = clear_commit(n)
-            os.system('git commit -m "'+n+'"')
-            print("----------------------------")
-            os.system("git push")
-        elif n == "n":
-            program(False)
-
+        programthree()
+    else:
+        programptwo()
+def programthree():
+    n = input("Are you complete program (y/n) : ")
+    if n == 'y':
+        os.system("git add index.html")
+        print("----------------------------")
+        n = input("Please enter git commit : ")
+        n = clear_commit(n)
+        os.system('git commit -m "'+n+'"')
+        print("----------------------------")
+        os.system("git push")
+    elif n == "n":
+        program(False)
+    else:
+        programthree()
 def clear_commit(name):
     name = name.replace('"', "'")
     return name
